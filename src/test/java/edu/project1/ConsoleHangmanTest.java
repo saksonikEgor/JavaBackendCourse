f7e0ca91860db9db5b081b9f769ed0d844c26bc7
Saksonik Egor <saksonik.egor@mail.ru>
2023-10-12 08:05:56 +0300

    Add structure for output testing methods

--- src/test/java/edu/project1/ConsoleHangmanTest.java
+++ src/test/java/edu/project1/ConsoleHangmanTest.java
@@ -3,41 +3,50 @@ package edu.project1;
 import edu.project1.util.ConsoleHangman;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConsoleHangmanTest {
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-    void function() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + "!!!");
-    }
-
-    @BeforeEach
-    public void setUp() {
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
-
     @Test
-    void functionTest() {
-        provideInput("Baeldung");
-
-        function();
-
-        assertEquals("Baeldung!!!\n", outputStreamCaptor.toString());
+    void printHelloWord() throws Exception {
+        ConsoleHangman consoleHangman = new ConsoleHangman();
+        assertEquals(tapSystemOut(consoleHangman::run), "");
     }
 
-    @AfterEach
-    public void tearDown() {
-        System.setOut(standardOut);
-    }
+//    void function() {
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//        System.out.println(input + "!!!");
+//    }
+//
+//    @BeforeEach
+//    public void setUp() {
+//        System.setOut(new PrintStream(outputStreamCaptor));
+//    }
+//
+//    @Test
+//    void functionTest() {
+//        provideInput("Baeldung");
+//
+//        function();
+//
+//        assertEquals("Baeldung!!!\n", outputStreamCaptor.toString());
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        System.setOut(standardOut);
+//    }
 
     void provideInput(String data) {
         ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
