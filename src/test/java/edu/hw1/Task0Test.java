70a0a2d8778bbcb35a9387be74200fc85a1f1cfa
Saksonik Egor <saksonik.egor@mail.ru>
2023-10-14 17:30:12 +0300

    Change contains body in printHelloWord method

--- src/test/java/edu/hw1/Task0Test.java
+++ src/test/java/edu/hw1/Task0Test.java
@@ -2,6 +2,7 @@ package edu.hw1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,6 +11,6 @@ public class Task0Test {
     @DisplayName("Проверка вывода строки \"Привет, мир!\"")
     void printHelloWord() throws Exception {
         assertTrue(tapSystemOut(Task0::printHelloWorld)
-            .contains("\u001B[m [main           ] edu.hw1.Task0   -- \u001B[32mПривет, мир!\u001B[m\n"));
+            .contains("Привет, мир!"));
     }
 }
