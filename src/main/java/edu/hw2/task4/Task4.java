36781d0dd2c1c48f4d307c80843750207f9b2c58
Saksonik Egor <saksonik.egor@mail.ru>
2023-10-14 14:20:59 +0300

    Add task4

---
+++ src/main/java/edu/hw2/task4/Task4.java
@@ -0,0 +1,21 @@
+package edu.hw2.task4;
+
+public class Task4 {
+    public static final String NO_ONE_CALLED_THIS_METHOD_MESSAGE = "no one called this method";
+
+    private Task4() {
+    }
+
+    public record CallingInfo(String className, String methodName) {
+    }
+
+    public static CallingInfo callingInfo(String methodName) {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element : stackTraceElements) {
+            if (element.getMethodName().equals(methodName)) {
+                return new CallingInfo(element.getClassName(), element.getMethodName());
+            }
+        }
+        return new CallingInfo(NO_ONE_CALLED_THIS_METHOD_MESSAGE, methodName);
+    }
+}
