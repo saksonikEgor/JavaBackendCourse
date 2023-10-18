6d179efbd311f6456bcc9d0859a29bcfa7457f1f
Saksonik Egor <saksonik.egor@mail.ru>
2023-10-18 10:36:34 +0300

    Remove CONNECTION_UPPER_BOUND check

--- src/main/java/edu/hw2/task3/Connection.java
+++ src/main/java/edu/hw2/task3/Connection.java
@@ -17,21 +17,14 @@ public sealed interface Connection extends AutoCloseable {
 
     final class FaultyConnection implements Connection {
         public static final String CONNECTION_EXCEPTION_MESSAGE = "Faulty execute";
-        public static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "Seed must be greater than 0";
         public static final int CONNECTION_UPPER_BOUND = 2;
         private final Random connectionRandom;
 
         public FaultyConnection() {
-            if (CONNECTION_UPPER_BOUND < 1) {
-                throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE);
-            }
             connectionRandom = new Random();
         }
 
         public FaultyConnection(Random connectionRandom) {
-            if (CONNECTION_UPPER_BOUND < 1) {
-                throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE);
-            }
             this.connectionRandom = connectionRandom;
         }
 
