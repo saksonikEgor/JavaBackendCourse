903131693b6a989cdbfdfcf9f74287776b607cec
Saksonik Egor <saksonik.egor@mail.ru>
2023-10-30 22:24:38 +0300

    Reformat code

--- src/test/java/edu/hw3/Task5Test.java
+++ src/test/java/edu/hw3/Task5Test.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.in;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class Task5Test {
