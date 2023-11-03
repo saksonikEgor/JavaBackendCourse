e6669d1d208ea0e2331a3cd6a430a258a532ad64
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-03 10:31:07 +0300

    Change task1 return format

--- src/main/java/edu/hw5/Task1.java
+++ src/main/java/edu/hw5/Task1.java
@@ -1,18 +1,22 @@
 package edu.hw5;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 
 public class Task1 {
-    private static final String PATTERN = "uuuu:M:d, hh:mm";
+    private static final String INPUT_DATA_PATTERN = "yyyy-MM-dd, HH:mm";
+    private static final String OUTPUT_AVR_DATA_FORMAT = "hh:mm";
     public static final String NULL_POINTER_EXCEPTION_MESSAGE = "String \"s\" cant be null";
     public static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "String \"s\" cant be empty";
 
     private Task1() {
     }
 
-    public static double calculateAvg(String s) {
+    public static String calculateAvg(String s) {
         if (s == null) {
             throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
         }
@@ -21,16 +25,18 @@ public class Task1 {
         }
 
         String[] ranges = s.split("\n");
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(PATTERN);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(INPUT_DATA_PATTERN);
         long sum = 0;
 
         for (String range : ranges) {
-            String[] dates = range.split("-");
+            String[] dates = range.split(" - ");
             LocalDateTime start = LocalDateTime.parse(dates[0].trim(), dateTimeFormatter);
             LocalDateTime end = LocalDateTime.parse(dates[1].trim(), dateTimeFormatter);
             sum += Duration.between(start, end).toMinutes();
         }
 
-        return (double) sum / ranges.length;
+
+        Duration duration  = Duration.ofMinutes(sum / ranges.length);
+        return String.format("%02d:%02d", duration.toHours(), duration.toMinutesPart());
     }
 }
