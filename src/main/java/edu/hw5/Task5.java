c47ef4c33f4b948179d3aaa02cf03a4b090f60c1
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-12 20:30:15 +0300

    Add task5

---
+++ src/main/java/edu/hw5/Task5.java
@@ -0,0 +1,17 @@
+package edu.hw5;
+
+public class Task5 {
+    private static final String PATTERN = "[А-Я][1-9]\\d{2}[А-Я]{2}[1-9]\\d{2}";
+    public static final String NULL_POINTER_EXCEPTION_MESSAGE = "String \"s\" cant be null";
+
+    private Task5() {
+    }
+
+    public static boolean isValidRussianLicensePlates(String s) {
+        if (s == null) {
+            throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+        }
+
+        return s.matches(PATTERN);
+    }
+}
