ab39f4c49d28d87c8d6243246df410d8741dc539
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-12 16:35:41 +0300

    Change return type to the Duration

--- src/main/java/edu/hw5/Task1.java
+++ src/main/java/edu/hw5/Task1.java
@@ -1,22 +1,18 @@
 package edu.hw5;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-import java.util.Date;
 
 public class Task1 {
     private static final String INPUT_DATA_PATTERN = "yyyy-MM-dd, HH:mm";
-    private static final String OUTPUT_AVR_DATA_FORMAT = "hh:mm";
     public static final String NULL_POINTER_EXCEPTION_MESSAGE = "String \"s\" cant be null";
     public static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "String \"s\" cant be empty";
 
     private Task1() {
     }
 
-    public static String calculateAvg(String s) {
+    public static Duration calculateAvg(String s) {
         if (s == null) {
             throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
         }
@@ -35,8 +31,6 @@ public class Task1 {
             sum += Duration.between(start, end).toMinutes();
         }
 
-
-        Duration duration  = Duration.ofMinutes(sum / ranges.length);
-        return String.format("%02d:%02d", duration.toHours(), duration.toMinutesPart());
+        return Duration.ofMinutes(sum / ranges.length);
     }
 }
