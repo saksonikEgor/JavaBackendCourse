2b5f69f0ae8ad6bbf5895777049075f128738a93
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-12 20:54:16 +0300

    Add task7

---
+++ src/main/java/edu/hw5/Task7.java
@@ -0,0 +1,17 @@
+package edu.hw5;
+
+public class Task7 {
+    private static final String PATTERN = "0[0-1]0";
+    public static final String NULL_POINTER_EXCEPTION_MESSAGE = "String \"s\" cant be null";
+
+    private Task7() {
+    }
+
+    public static boolean isStringValid(String s) {
+        if (s == null) {
+            throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+        }
+
+        return s.matches(PATTERN);
+    }
+}
