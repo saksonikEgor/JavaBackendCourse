b073d8ec58cd2e167e93523483b49cc6d3a38066
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-13 14:28:10 +0300

    Break the regular expression into three parts

--- src/main/java/edu/hw5/Task7.java
+++ src/main/java/edu/hw5/Task7.java
@@ -2,7 +2,9 @@ package edu.hw5;
 
 public class Task7 {
     public static final String NULL_POINTER_EXCEPTION_MESSAGE = "String \"s\" cant be null";
-    private static final String PATTERN = "0[0-1]0";
+    private static final String FIRST_PATTERN = "[0-1]{2}0[0-1]*";
+    private static final String SECOND_PATTERN = "([0-1])[0-1]*\\1";
+    private static final String THIRD_PATTERN = "[0-1]{1,3}";
 
     private Task7() {
     }
@@ -12,6 +14,6 @@ public class Task7 {
             throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
         }
 
-        return s.matches(PATTERN);
+        return s.matches(FIRST_PATTERN) && s.matches(SECOND_PATTERN) && s.matches(THIRD_PATTERN);
     }
 }
