9d8cf18b9d4ca0f917fe6b9580d1f0ed9a576f38
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-19 20:13:34 +0300

    Add tests for util class

---
+++ src/test/java/edu/project3/parser/LogParserTest.java
@@ -0,0 +1,12 @@
+package edu.project3.parser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LogParserTest {
+    @Test
+    @DisplayName("Чтение логов из файла")
+    void parseLog() {
+
+    }
+}
