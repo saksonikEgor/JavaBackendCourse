4f45b29537a5af5eadfaf071f94f559b26455859
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-19 21:00:39 +0300

    Fix bug

--- src/test/java/edu/hw6/Task6Test.java
+++ src/test/java/edu/hw6/Task6Test.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Task6Test {
     @DisplayName("Сканирование портов")
     @Test
     void scanPorts() {
-        assertFalse(Task6.isPortBusy(8080));
+        assertTrue(Task6.isPortBusy(8080));
 
         assertDoesNotThrow(Task6::scanPorts);
     }
