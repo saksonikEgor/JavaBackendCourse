5580ec672514e8ef99a0f3b419f549d7813df897
Saksonik Egor <saksonik.egor@mail.ru>
2023-11-24 21:14:06 +0300

    Initial task2

---
+++ src/main/java/edu/hw8/task2/FixedThreadPool.java
@@ -0,0 +1,25 @@
+package edu.hw8.task2;
+
+public class FixedThreadPool implements ThreadPool {
+//    private Thread[] threads;
+//
+//    public Thread[] create(int threadCount) {
+//        threads = new Thread[threadCount];
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            threads[i] = new Thread();
+//        }
+//
+//        return threads;
+//    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+
+    }
+}
