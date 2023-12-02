605414407cc742507f2d921a6428fc479d3ab5de
Saksonik Egor <saksonik.egor@mail.ru>
2023-12-02 19:41:03 +0300

    Add task2

--- src/main/java/edu/hw8/task2/FixedThreadPool.java
+++ src/main/java/edu/hw8/task2/FixedThreadPool.java
@@ -1,25 +1,43 @@
 package edu.hw8.task2;
 
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.Stream;
+
 public class FixedThreadPool implements ThreadPool {
-//    private Thread[] threads;
-//
-//    public Thread[] create(int threadCount) {
-//        threads = new Thread[threadCount];
-//
-//        for (int i = 0; i < threadCount; i++) {
-//            threads[i] = new Thread();
-//        }
-//
-//        return threads;
-//    }
+    private final Thread[] threads;
+    private final BlockingQueue<Runnable> blockingQueue;
+
+    public FixedThreadPool(int threadCount) {
+        blockingQueue = new ArrayBlockingQueue<>(threadCount);
+        threads = Stream.generate(
+                () -> new Thread(
+                    () -> {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                blockingQueue.take().run();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                )
+            ).limit(threadCount)
+            .toArray(Thread[]::new);
+    }
 
     @Override
     public void start() {
-
+        Arrays.stream(threads).forEach(Thread::start);
     }
 
     @Override
     public void execute(Runnable runnable) {
-
+        try {
+            blockingQueue.put(runnable);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
