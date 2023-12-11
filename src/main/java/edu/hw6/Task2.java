d557b94eb5e1a67fc1adc5ba756860eb6a147763
Saksonik Egor <saksonik.egor@mail.ru>
2023-12-11 13:27:11 +0300

    Fix bug in task 2

--- src/main/java/edu/hw6/Task2.java
+++ src/main/java/edu/hw6/Task2.java
@@ -12,7 +12,8 @@ public class Task2 {
     public static final String NULL_POINTER_EXCEPTION_MESSAGE = "Path cant be null";
     public static final String FILE_NOT_FOUND_EXCEPTION_MESSAGE = "File with path \"{}\" does not exist";
     private static final String CLONE_SUFFIX = " - копия";
-    private static final char EXTENSION_CHARACTER = '.';
+    private static final char EXTENSION_DELIMITER_CHARACTER = '.';
+    private static final String PATTERN = "(FILENAME)( - копия( \\(\\d+\\))?)?EXTENSION";
 
     private Task2() {
     }
@@ -25,19 +26,31 @@ public class Task2 {
             throw new FileNotFoundException(FILE_NOT_FOUND_EXCEPTION_MESSAGE);
         }
 
+        String fileFullName = path.getFileName().toString();
+        int extensionCharIdx = fileFullName.indexOf(EXTENSION_DELIMITER_CHARACTER);
+        String fileName = fileFullName.substring(0, extensionCharIdx);
+        String fileExtension = fileFullName.substring(extensionCharIdx);
+
+        String cloneName;
+        int fileId = 1;
+        if (!fileName.contains(CLONE_SUFFIX)) {
+            cloneName = fileName + CLONE_SUFFIX;
+        } else {
+            cloneName = fileName;
+            fileName = fileName.substring(0, fileName.indexOf(CLONE_SUFFIX));
+        }
+
+        String pattern = PATTERN.replaceAll("FILENAME", fileName)
+            .replaceAll("EXTENSION", fileExtension);
+
         try (Stream<Path> paths = Files.list(path.getParent())) {
+
             Set<String> fileNames = paths
                 .map(p -> p.getFileName().toString())
-                .map(name -> name.substring(0, name.indexOf(EXTENSION_CHARACTER)))
+                .filter(name -> name.matches(pattern))
+                .map(name -> name.substring(0, name.indexOf(EXTENSION_DELIMITER_CHARACTER)))
                 .collect(Collectors.toSet());
 
-            String fileFullName = path.getFileName().toString();
-            int extensionCharIdx = fileFullName.indexOf(EXTENSION_CHARACTER);
-            String fileName = fileFullName.substring(0, extensionCharIdx);
-            String fileExtension = fileFullName.substring(extensionCharIdx);
-
-            String cloneName = fileName + CLONE_SUFFIX;
-            int fileId = 1;
             while (fileNames.contains(cloneName)) {
                 cloneName = concatFileNameAndId(fileName, fileId);
                 fileId++;
@@ -48,10 +61,6 @@ public class Task2 {
     }
 
     private static String concatFileNameAndId(String fileName, int fileId) {
-        return fileName
-            .concat(CLONE_SUFFIX)
-            .concat(" (")
-            .concat(String.valueOf(fileId))
-            .concat(")");
+        return fileName + CLONE_SUFFIX + " (" + fileId + ")";
     }
 }
