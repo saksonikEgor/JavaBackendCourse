0e1778dab73eded1952371124ea9f7a092cc0d0e
Saksonik Egor <saksonik.egor@mail.ru>
2023-12-16 15:11:10 +0300

    Add task 2 tests

---
+++ src/test/java/edu/hw11/Task2Test.java
@@ -0,0 +1,31 @@
+package edu.hw11;
+
+import edu.hw11.task2.Task2;
+import edu.hw11.task2.modelSample.Adder;
+import edu.hw11.task2.modelSample.Multiplier;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task2Test {
+    @Test
+    @DisplayName("Изменение поведения метода класса")
+    void runtimeMethodDelegating()
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        final int A = 3;
+        final int B = 10;
+
+        assertEquals(
+            Multiplier.multiply(A, B),
+            ((Adder) Task2.changeMethodImplementation(
+                Adder.class,
+                Adder.class.getDeclaredMethod("sum", int.class, int.class),
+                Multiplier.class
+            )).sum(A, B)
+        );
+    }
+
+}
